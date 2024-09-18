db4532083aca6b2717832060cbfe2f51f29bb889
AadiManchekar <aadimanchekar2002@gmail.com>
2024-09-18 12:13:48 -0700

    adding public & private route tables

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -24,7 +24,7 @@ resource "aws_subnet" "private_zone1" {
 
   tags = {
     "Name"                                      = "${var.cluster_name}-private-${local.availability_zones[0]}"
-    "kubernetes.io/role/internal-elb"           = "1" 
+    "kubernetes.io/role/internal-elb"           = "1"
     "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
 }
@@ -68,3 +68,75 @@ resource "aws_subnet" "public_zone2" {
   }
 }
 
+/*
+We would need two (public & private) route tables
+Why?
+All the workloads will run in private subnet and they would need access to internet inorder to download packages etc
+For which internet-bound traffic arising from private subnets (where our workloads run) will be directed towards Nat gateway which is placed in public subnet.
+Nat gateway helps in handling outbound traffic while keeping the instances private.
+
+NOTE: Internet Gateway only allows traffic for public subnets
+
+OUTBOUND Traffic from Private Subnet to the Internet
+1. Instance in Private Subnet initiates a request eg an API call
+2. Private Route Table routes the outbound traffic with the destination 0.0.0.0/0 (any address on the internet) through the NAT Gateway.
+3. NAT Gateway is deployed in the public subnet and has a public IP address (Elastic IP) (static ip). The NAT Gateway performs network address translation, changing the source IP of the private instance to its own public IP.
+4. Internet Gateway forwards the request from the NAT Gateway to the internet.
+In this way a request from private subnet is reached to internet (NOTE here the source IP will be of Nat Gatway)
+
+Inbound Traffic from the Internet to Private 
+1. Internet will respond back to source IP (which is our NAT gateway) but it first its internet gateway
+2. The IGW forwards the traffic directly to the instance in the public subnet i.e to our NAT gateway
+3. Nat gateway does a reverse NAT and then forwards the traffic to the desired private instance in private subnet
+*/
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-private-route-table"
+  }
+
+  depends_on = [aws_nat_gateway.nat]
+}
+
+resource "aws_route_table_association" "private_zone1" {
+  subnet_id      = aws_subnet.private_zone1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_zone2" {
+  subnet_id      = aws_subnet.private_zone2.id
+  route_table_id = aws_route_table.private.id
+}
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-public-route-table"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table_association" "public_zone1" {
+  subnet_id      = aws_subnet.public_zone1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_zone2" {
+  subnet_id      = aws_subnet.public_zone2.id
+  route_table_id = aws_route_table.public.id
+}
